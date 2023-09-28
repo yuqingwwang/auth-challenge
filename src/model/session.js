ee222f5fc0c037032f8260d2ec8c6053c4adfa15
Yuqing Wang <bibolikepeaches@gmail.com>
2023-09-28 15:53:07 +0100

    passing test 1

--- src/model/session.js
+++ src/model/session.js
@@ -1,9 +1,16 @@
 const db = require("../database/db.js");
+const crypto = require("node:crypto");
 
-const insert_session = db.prepare(`SELECT 1`);
+const insert_session = db.prepare(/*SQL*/`
+INSERT INTO sessions
+VALUES($id, $user_id, DATE('now', '+7 day'), DATETIME('now'))
+`);
 
 function createSession(user_id) {
-  // to-do
+  // Generate a strong, long, random string to use as the session ID
+  const id = crypto.randomBytes(18).toString('base64');
+  insert_session.run({ id, user_id })
+  return id
 }
 
 const select_session = db.prepare(`

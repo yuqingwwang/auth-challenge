9a2fbe570dfe162816e3b242805010b993df3c57
Yuqing Wang <bibolikepeaches@gmail.com>
2023-09-29 13:42:17 +0100

    test 2 passed

--- src/routes/sign-up.js
+++ src/routes/sign-up.js
@@ -1,4 +1,8 @@
 const { Layout } = require("../templates.js");
+const bcrypt = require("bcryptjs");
+const { createUser } = require("../model/user.js");
+const { createSession } = require("../model/session.js");
+
 
 function get(req, res) {
   const title = "Create an account";
@@ -27,7 +31,6 @@ function post(req, res) {
   if (!email || !password) {
     res.status(400).send("Bad input");
   } else {
-    res.send("to-do");
     /**
      * [1] Hash the password
      * [2] Create the user in the DB
@@ -35,6 +38,19 @@ function post(req, res) {
      * [4] Set a cookie with the session ID
      * [5] Redirect to the user's confession page (e.g. /confessions/3)
      */
+    bcrypt.hash(password, 12).then((hash) =>
+    {
+      const user = createUser(email, hash)
+      const sessionID = createSession(user.id)
+      res.cookie("sid", sessionID, {
+        signed: true,
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+        sameSite: "lax",
+        httpOnly: true,
+      });
+      res.redirect(`/confessions/${user.id}`);
+    }
+    );
   }
 }
 
